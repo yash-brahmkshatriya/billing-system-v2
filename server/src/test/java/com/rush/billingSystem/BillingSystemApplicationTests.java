729a16d24ade5abf5ea9d2fd49092a12e904a178
Yash Brahmkshatriya <yash311000@gmail.com>
2022-02-22 22:33:39 +0530

    [ADD] initial boilerplate

---
+++ server/src/test/java/com/rush/billingSystem/BillingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rush.billingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
