2d83786fa2bf71ee003d30717d592c6aa149ae56
Yash Brahmkshatriya <yash311000@gmail.com>
2022-10-15 22:59:10 +0530

    updated netlify.toml and updated rou express routes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
 
-const api = require("./api");
-const serverUtil = require("./utils/serverUtil");
+const api = require('./api');
+const serverUtil = require('./utils/serverUtil');
 
 const app = express();
 
@@ -15,7 +15,10 @@ const startApp = async () => {
   app.use(express.json());
   app.use(cookieParser());
 
-  app.use("/api", api);
+  app.get('/', (req, res) =>
+    res.status(200).send('Billing system v2 Express erver running...')
+  );
+  app.use('/api', api);
 
   app.listen(PORT, () => {
     console.log(`Billing System v2 API - up and running on ${PORT}`);
